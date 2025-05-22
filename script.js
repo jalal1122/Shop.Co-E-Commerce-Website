0d0fdddc3f69013cfae28792bb029490da242212
muhammaduzair432 <mikykhan702@gmail.com>
2025-05-22 19:01:43 +0500

    Refactor swiper configuration and enhance CSS utility classes
    - Removed looping functionality from Swiper initialization.
    - Adjusted breakpoints for responsive slides per view in Swiper.
    - Added new CSS utility classes for margin, padding, width, height, and flex properties.
    - Introduced gradient background utilities and improved placeholder text color handling.
    - Added support for outline and filter properties in CSS.
    - Enhanced media query handling for responsive design.

--- script.js
+++ script.js
@@ -190,7 +190,6 @@ async function main() {
   const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
-    loop: true,
     speed: 400,
     spaceBetween: 100,
     slidesPerView: 1,
@@ -204,19 +203,22 @@ async function main() {
     breakpoints: {
       // when window width is >= 320px
       320: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 20,
       },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 3,
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 2,
         spaceBetween: 30,
       },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 4,
+      1024:{
+        slidesPerView: 3,
         spaceBetween: 40,
       },
+      1440:{
+        slidesPerView: 4,
+        spaceBetween: 50,
+      }
     },
   });
 }
